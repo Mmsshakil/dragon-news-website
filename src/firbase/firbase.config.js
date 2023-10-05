454ef1c65d4c2fb3ba67ad845ae2b789b21902d5
Mmsshakil <softhandshakil@gmail.com>
2023-10-06 01:41:06 +0600

    firebase and route add

---
+++ src/firbase/firbase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDg_UmSOe0fztwCJxuC5DJ-RVHiaJmDJLs",
+  authDomain: "dragon-news-website-bf482.firebaseapp.com",
+  projectId: "dragon-news-website-bf482",
+  storageBucket: "dragon-news-website-bf482.appspot.com",
+  messagingSenderId: "999389580451",
+  appId: "1:999389580451:web:78184a021fdb2cbac14316"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
