240602a6bb6dcf2508df01f4b91cdadfd0c03ee7
Mmsshakil <softhandshakil@gmail.com>
2023-10-06 19:42:26 +0600

    left side category showing from json data

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const LeftSideNav = () => {
+
+    const [categories, setcategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setcategories(data))
+    },[])
+
     return (
         <div>
-            <h2>left side nav</h2>
+            <h2 className="text-xl font-semibold">All Caterogy</h2>
+            {
+                categories.map(category => <Link to={`/category/${category.id}`} className="block py-3 pl-10 text-xl font-medium" key={category.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
