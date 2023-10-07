6f9af050570ed5c88a5a44bdddde6571e9e7a708
Mmsshakil <softhandshakil@gmail.com>
2023-10-07 12:54:15 +0600

    env veriable for hide the package json file

--- src/firbase/firbase.config.js
+++ src/firbase/firbase.config.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDg_UmSOe0fztwCJxuC5DJ-RVHiaJmDJLs",
-  authDomain: "dragon-news-website-bf482.firebaseapp.com",
-  projectId: "dragon-news-website-bf482",
-  storageBucket: "dragon-news-website-bf482.appspot.com",
-  messagingSenderId: "999389580451",
-  appId: "1:999389580451:web:78184a021fdb2cbac14316"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
